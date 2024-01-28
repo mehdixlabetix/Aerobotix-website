5f372564c632f4bd3ce7826db3212589514c841a
Mehdi Cherif <mehdi.cherif@insat.ucar.tn>
2024-01-29 00:01:14 +0100

    :art: styling

--- src/components/Eurobot.jsx
+++ src/components/Eurobot.jsx
@@ -4,20 +4,19 @@ import {Button, Heading, HStack, Image, Text, VStack} from "@chakra-ui/react";
 
 const Eurobot = () => {
     return (
-        <FullScreenSection  backgroundColor="white" id='eurobot-section' height={["400px","100vh"]}>
+        <FullScreenSection  backgroundColor="#111512" id='eurobot-section' height={["400px","100vh"]}>
             <HStack spacing={[2,8]} width="100%" height="100%" justifyContent="center" alignItems="center">
                 <Image width={["68%","50%"]} height="100%" src="/euro.jpg" alt="Eurobot"/>
                 <VStack spacing={[2,24]}>
-                    <Heading as="h1" size={["lg","xl"]}>Eurobot</Heading>
+                    <Heading color={"white"} as="h1" size={["lg","xl"]}>Eurobot</Heading>
                    <VStack spacing={8} marginTop={["0%","8%"]} marginBottom={["0%","-7%"]}>
-                       <Text w={['80%','100%']} fontSize={[8,16]}>
+                       <Text color={"white"} w={['80%','100%']} fontSize={[8,16]}>
                         Eurobot: is a European robotics competition that pits teams of students with a passion for robotics
                         against each other. Teams design, build and program autonomous robots to perform specific tasks on a
                         defined playing field. It's an opportunity for participants to apply their knowledge of engineering and
                         computer science while fostering team spirit.
                     </Text>
-                       <a  href='https://www.eurobot.org'><Button marginLeft={['10%','0%']} width={['80%','100%']} colorScheme="blackAlpha">View More</Button></a></VStack>
-                    <Image  src="/robot2.jpg" alt="robot" width={["60%","30%"]} height="10%"/>
+                       <a  href='https://www.eurobot.org'><Button marginLeft={['10%','0%']} width={['80%','100%']} colorScheme="whiteAlpha">View More</Button></a></VStack>
                 </VStack>
             </HStack>
         </FullScreenSection>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,13 +5,13 @@ import {Heading, Text, VStack,Image} from "@chakra-ui/react";
 const Intro = () => {
     return (
         <FullScreenSection style={{
-            backgroundColor:"white",
+            backgroundColor:"#111512",
             height:"100vh"
         }}   id="intro-section" justifyContent="center" alignItems="center">
            < VStack spcaing={10}>
-            <Heading marginTop="5%" fontSize={["xl","5xl"]} backgroundColor="white">A little introduction</Heading>
+            <Heading marginTop="5%" color={'white'} fontSize={["xl","5xl"]} >A little introduction</Heading>
             <Text id="landing-text" margin="7% 4% 3% 4%"
-                  style={{fontFamily: 'cursive', whiteSpace: 'pre-wrap',backgroundColor:"white" }} fontSize={["md","xl"]}>
+                  style={{fontFamily: 'cursive', whiteSpace: 'pre-wrap',color:"white" }} fontSize={["md","xl"]}>
                 The AeRobotiX club is a non-profit science club.
                 It is the result of the merger of the Mechatronics and CCE of INSAT.
                 AeRobotiX operates in the realization of robotic and automated applications having utility in vital
@@ -25,7 +25,7 @@ const Intro = () => {
                 The club also offers its members the opportunity to participate in national and international
                 competitions, which allows them to acquire a lot of experience and skills.
             </Text>
-               <Image  height="200px" src="/robot.jpg"/>
+               <Image  height="200px" src="/robot.png"/>
         </VStack>
         </FullScreenSection>
     )
