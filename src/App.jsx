fd1a88306f65ec8e2814d2b379777850fa790942
Mehdi Cherif <mehdi.cherif@insat.ucar.tn>
2024-02-18 13:39:40 +0100

    :zap: lazyLoaded ImageSlider

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import LandingPage from "./components/LandingPage.jsx";
 import Intro from "./components/Intro.jsx";
 import Particles from "./components/particles.jsx";
 import Axis from "./components/Axis.jsx";
-import NewImageSlider from "./components/ImageSlider.jsx";
 
 function App() {
     const LazyEurobot = lazy(() => import('./components/Eurobot.jsx'))
     const LazyTeam = lazy(() => import('./components/Team.jsx'))
     const LazyEvents = lazy(() => import('./components/Events.jsx'))
+    const LazyImageSlider = lazy(() => import('./components/ImageSlider.jsx'))
     return (
         <>
             <ChakraProvider>
@@ -21,8 +21,8 @@ function App() {
                 <LandingPage/>
                 <Intro/>
                 <Axis/>
-                <NewImageSlider/>
                 <Suspense fallback={<div>Loading...</div>}>
+                    <LazyImageSlider/>
                     <LazyEurobot/>
                 </Suspense>
                 <Suspense fallback={<div>Loading...</div>}>
