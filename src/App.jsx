f20eba0269f767582705007bc2ce50451c702224
Mehdi Cherif <mehdi.cherif@insat.ucar.tn>
2024-02-18 14:29:14 +0100

    :zap: reduced image quality

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,22 @@ import Footer from "./components/Footer.jsx";
 import LandingPage from "./components/LandingPage.jsx";
 import Intro from "./components/Intro.jsx";
 import Particles from "./components/particles.jsx";
-import Axis from "./components/Axis.jsx";
 
 function App() {
     const LazyEurobot = lazy(() => import('./components/Eurobot.jsx'))
     const LazyTeam = lazy(() => import('./components/Team.jsx'))
     const LazyEvents = lazy(() => import('./components/Events.jsx'))
     const LazyImageSlider = lazy(() => import('./components/ImageSlider.jsx'))
+    const LazyAxis = lazy(() => import('./components/Axis.jsx'))
     return (
         <>
             <ChakraProvider>
                 <Header/>
                 <LandingPage/>
                 <Intro/>
-                <Axis/>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <LazyAxis/>
+                </Suspense>
                 <Suspense fallback={<div>Loading...</div>}>
                     <LazyImageSlider/>
                     <LazyEurobot/>
